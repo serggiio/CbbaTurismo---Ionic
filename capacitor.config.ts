8976f005cf1fa846cbf4b6e2ef0e753e127b8f4e
sergio fernandez <sergiocep2010@gmail.com>
2021-08-08 19:14:59 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CBBATurismo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
